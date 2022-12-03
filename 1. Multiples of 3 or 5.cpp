6c102c74bec77b78638a5869114b1e5be1cd3d00
viksant <101194286+viksant@users.noreply.github.com>
2022-12-03 01:43:26 +0100

    Update 1. Multiples of 3 or 5.cpp

--- 1. Multiples of 3 or 5.cpp
+++ 1. Multiples of 3 or 5.cpp
@@ -1,3 +1,9 @@
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
 #include <iostream>
 #include <vector>
 
@@ -23,3 +29,7 @@ int main ()
 
 	return 0;
 }
+
+/*
+Output -> Result = 233168
+*/
