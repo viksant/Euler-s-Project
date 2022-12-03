928e39608cd83b08449b4ff5fde1342f2c354b99
viksant <101194286+viksant@users.noreply.github.com>
2022-12-03 01:38:41 +0100

    Create 3. Largest prime factor.cpp

---
+++ 3. Largest prime factor.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main ()
+{
+	long long number = 600851475143;
+
+	vector<int>multiples;
+
+	int i=2;
+
+	while (number != 1){
+		while (number % i == 0 ) {
+			multiples.push_back(i);
+			number = number / i;
+			if (number % i != 0)
+				i++;
+			if (number == 1)
+				break;
+		}
+		i++;
+	}
+
+	for (const int primes : multiples)
+		cout << primes <<endl;
+
+	return 0;
+}
